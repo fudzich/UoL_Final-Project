d24b44bc6a75d39ec1494ab12300fd9036938a93
fudzich <vsevolod656@gmail.com>
2025-12-16 21:58:50 +0300

    Add PCG Arena generator

---
+++ Final Project/Assets/Scripts/ArenaGenerator.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaGenerator : MonoBehaviour
+{
+
+    public class Cell{
+        public bool visited = false;
+        public bool[] status = new bool[2];
+    }
+
+    public Vector2 size;
+    public int startPos = 0;
+    public GameObject tile;
+    public Vector2 offset;
+
+    List<Cell> board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MazeGenerator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateArena(){
+        for (int i = 0; i < size.x; i++){
+            for (int j = 0; j < size.y; j++){
+
+               var newTile = Instantiate(tile, new Vector3(i*offset.x,0,-j*offset.y), Quaternion.identity, transform);
+               //newTile.GetComponent<TileBehaviour>().UpdateTile(board[Mathf.FloorToInt(i+j*size.x)].status);
+            }
+        }
+    }
+
+    void MazeGenerator(){
+        board = new List<Cell>();
+        
+        for (int i = 0; i < size.x; i++){
+            for (int j = 0; j < size.y; j++){
+                board.Add(new Cell());
+            }
+        }
+
+        int currentCell = startPos;
+
+        Stack<int> path = new Stack<int>();
+        int k = 0;
+
+        while(k < 1000){
+            k++;
+
+            board[currentCell].visited = true;
+
+            List<int> neighbours = CheckNeighbours(currentCell);
+
+            if (neighbours.Count == 0){
+                if (path.Count == 0){
+                    break;
+                }
+                else{
+                    currentCell = path.Pop();
+                }
+            }
+            else{
+                path.Push(currentCell);
+
+                int newCell = neighbours[Random.Range(0, neighbours.Count)];
+
+                //board[currentCell].status[(Random.Range(0, 2))] = true;
+            }
+        }
+        GenerateArena();
+    }
+
+    List<int> CheckNeighbours(int cell){
+        List<int> neighbours = new List<int>();
+
+        //check up neighbour
+        if(cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited){
+            neighbours.Add(Mathf.FloorToInt(cell - size.x));
+        }
+        //check down neigbour
+        if(cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited){
+            neighbours.Add(Mathf.FloorToInt(cell + size.x));
+        }
+        //check right neigbour
+        if((cell + 1) % size.x != 0 && !board[Mathf.FloorToInt(cell + 1)].visited){
+            neighbours.Add(Mathf.FloorToInt(cell + 1));
+        }
+        //check legt neigbour
+        if(cell % size.x != 0 && !board[Mathf.FloorToInt(cell - 1)].visited){
+            neighbours.Add(Mathf.FloorToInt(cell - 1));
+        }
+
+        return neighbours;
+    }
+}
---
+++ Final Project/Assets/Scripts/TileBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour
+{
+
+    public GameObject[] towers; // 0 - No tower 1 - Sphere tower 2 - Cube tower
+
+    public void UpdateTile(bool[] status)
+    {
+        for(int i = 0; i < status.Length; i++)
+        {
+            towers[i].SetActive(status[i]);
+        }
+    }
+}
